f2f34c954abf5b72882e52523792a3c24b241a98
coolden <31628228+coolden@users.noreply.github.com>
2019-08-02 02:28:04 +0300

    процедурный сепаратизм

--- code/shitcode/hule/tts/tts.py
+++ code/shitcode/hule/tts/tts.py
@@ -17,13 +17,13 @@ for line in file:
 file.close()
 
 msg = params[0]
-client = params[1]
+name = params[1]
 lng = params[2]
 
 msg = unquote_plus(msg, 'cp1251')
 
 tts = gTTS(msg, lang=lng)
-tts.save(str(path)+"/conv/"+str(client)+".mp3")
+tts.save(str(path)+"/conv/"+str(name)+".mp3")
 
-subprocess.run("ffmpeg -i "+str(path)+"/conv/"+str(client)+".mp3 -vn -ar 44100 -ac 2 -b:a 64k "+str(path)+"/lines/"+str(client)+".ogg -y", shell=True)
+subprocess.run("ffmpeg -i "+str(path)+"/conv/"+str(name)+".mp3 -vn -ar 44100 -ac 2 -b:a 64k "+str(path)+"/lines/"+str(name)+".ogg -y", shell=True)
 
