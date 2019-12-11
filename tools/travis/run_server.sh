7a58782e03a1fa74ddc54f5534f38678aa7b91d0
Archemagus <32466328+Archemagus@users.noreply.github.com>
2019-12-11 05:26:00 +0300

    Гусь работает по ночам

--- tools/travis/run_server.sh
+++ tools/travis/run_server.sh
@@ -5,7 +5,7 @@ tools/deploy.sh travis_test
 mkdir travis_test/config
 
 #test config
-cp tools/travis/travis_config.txt travis_test/config/config.txt
+cp tools/travis/travis_config.txt travis_test/cfg/config.txt
 
 cd travis_test
 ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
